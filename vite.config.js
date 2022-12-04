2f7fd4192d451941128286202bc907661e3ca027
kulotsystems <kulotsystems@gmail.com>
2022-12-04 16:03:29 +0800

    Deploy.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()]
-})
+export default ({ mode }) => {
+    const config = {
+        plugins: [react()]
+    };
+
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+    const SKIP_BASE_PATH = process.env.VITE_SKIP_BASE_BATH;
+    if (SKIP_BASE_PATH === undefined || SKIP_BASE_PATH === 'false')
+        config.base = '/react-thenetninja/';
+
+    return defineConfig(config);
+}
