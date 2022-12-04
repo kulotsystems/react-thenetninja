47ac4ca7464d07ca76888f99aa5b5b4391655400
kulotsystems <kulotsystems@gmail.com>
2022-12-04 10:18:13 +0800

    Lesson 5: Multiple Components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import './App.css';
+import Home   from './Home.jsx';
+import Navbar from './Navbar.jsx';
 
 function App() {
-    const title  = 'Welcome to the new blog';
-    const likes  = 50;
-    const link   = 'http://www.kulotsystems.com/portfolio';
-
     return (
         <div className="App">
+            <Navbar/>
             <div className="content">
-                <h1>{ title }</h1>
-                <p>Liked { likes } times</p>
-
-                <p>{ 10 }</p>
-                <p>{ "hello ninjas!" }</p>
-                <p>{ [1, 2, 3, 4, 5] }</p>
-                <p>{ Math.random() * 10 }</p>
-
-                <a href={ link }>Kulot Systems</a>
+                <Home/>
             </div>
         </div>
     );
