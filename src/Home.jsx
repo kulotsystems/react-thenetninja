2d3e2c5772c8b709b13f05b961fd88e059afbd28
kulotsystems <kulotsystems@gmail.com>
2022-12-04 14:46:09 +0800

    Lesson 10: Outputting Lists.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,20 @@
 import { useState } from 'react';
 
 const Home = () => {
-    const [name, setName] = useState('mario');
-    const [age, setAge]   = useState(25);
-
-    const handleClick = (e) => {
-        setName('luigi');
-        setAge(30);
-        console.log(name);
-    }
+    const [blogs, setBlogs] = useState([
+        { id: 1, title: 'My New Website'  , body: 'lorem ipsum...', author: 'mario' },
+        { id: 2, title: 'Welcome Party!'  , body: 'lorem ipsum...', author: 'yoshi' },
+        { id: 3, title: 'Web Dev Top Tips', body: 'lorem ipsum...', author: 'mario' }
+    ]);
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old.</p>
-            <button onClick={handleClick}>Click Me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h1>{blog.title}</h1>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 };
