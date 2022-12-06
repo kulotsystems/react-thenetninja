a66e54d8948441615d3de0aedfbf76927545839c
kulotsystems <kulotsystems@gmail.com>
2022-12-06 13:49:57 +0800

    Lesson 28: Submit Events.

--- src/Create.jsx
+++ src/Create.jsx
@@ -5,10 +5,17 @@ const Create = () => {
     const [body, setBody]     = useState('');
     const [author, setAuthor] = useState('Mario');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const blog = { title, body, author };
+
+        console.log(blog);
+    };
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="blog-title">Title:</label>
                 <input
                     type="text"
