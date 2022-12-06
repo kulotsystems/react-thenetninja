a3ff7d40186aca3f439532b6ad9d242adf6ab0f3
kulotsystems <kulotsystems@gmail.com>
2022-12-06 14:15:00 +0800

    Lesson 30: Programmatic Redirects.

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Create = () => {
     const [title, setTitle]   = useState('');
     const [body, setBody]     = useState('');
     const [author, setAuthor] = useState('Mario');
     const [isPending, setIsPending] = useState(false);
+    const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,6 +20,7 @@ const Create = () => {
         }).then(response => {
             console.log(response);
             setIsPending(false);
+            navigate('/');
         });
      };
 
