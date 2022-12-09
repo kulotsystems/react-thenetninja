2a645e0a4a622f07e09fcdd881f24bb308210a66
kulotsystems <kulotsystems@gmail.com>
2022-12-09 18:57:06 +0800

    Introduce routeBase.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,12 @@
+import routes from './routes.js';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href={routes.home}>Home</a>
+                <a href={routes.create}>New Blog</a>
             </div>
         </nav>
     );
